e4d252d8d5467723da3a1984f3dfc7f976db981e
Jean <jean.marinsek@realis.si>
2019-11-26 21:00:17 +0100

    Delujoče importanje podatkov ter spremenjene CSV datoteke

--- ImportMTproject.py
+++ ImportMTproject.py
@@ -2,6 +2,7 @@ import json
 import csv
 from elasticsearch import Elasticsearch, helpers
 from _codecs import *
+from pathlib import Path
 
 es = Elasticsearch();
 
@@ -9,39 +10,45 @@ es = Elasticsearch();
 #     proracun_mappings = json.loads(filet.read())
 #     es.indices.create(index='proracun1', body=proracun_mappings)
 
-
+elastic_id = 1
 
 def read_csv(filename, root_key='BLC_ID', as_list=False):
-    #filename.encode('utf-8').decode('cp1252', errors='backslashreplace')
+    if es.indices.exists(index="posebni") is False:
+        with open('index.json', 'r') as f:
+            es_mappings = json.loads(f.read())
+            es.indices.create(index='posebni', body=es_mappings)
 
-    with open(filename, 'r', encoding="utf-8-sig") as f:
-        r = csv.DictReader(f)
-        helpers.bulk(es, r, index='test_index3', doc_type='_doc')
-
-
+    leto, number = str(Path(filename).stem).split("_")
+    global elastic_id
     with open(filename, 'r', encoding="utf-8-sig") as f:
             #print(f.read())
             
             c = list(csv.DictReader(f, delimiter=';'))
-            #print(c)
-            elastic_id = 1
-            #actions = []
             for row in c:
-                row['Leto'] = 2015
+                row['Leto'] = int(number)
                 row['Znesek'] = float(row['Znesek'].replace(",", "."))
-                row['_index'] = "test_index3"
+                row['NADSKUPINA_ID'] = int(row['NADSKUPINA_ID'])
+                row['SPU_ID'] = int(row['SPU_ID'])
+                row['PU_ID'] = int(row['PU_ID'])
+                row['POL_ID'] = int(row['POL_ID'])
+                row['PRG_ID'] = int(row['PRG_ID'])
+                row['POD_ID'] = int(row['POD_ID'])
+
+                row['_index'] = "posebni"
                 row['_type'] = "_doc"
-                row['_id'] = elastic_id #elastic_id naj bo globalen in ga povečuj!!
-                elastic_id+=1 #pozor, elastic_id naj bo definiran izven funkcije, sicer bo vedno šel od 1!
+                row['_id'] = int(elastic_id)
+                elastic_id+=1
 
                 #print(row)
                 actions.append(row)
+            # helpers.bulk(es, c, index='test_index3')
     print("KONEC READA")
-            
-            
+
 
 actions = []
 
+#primer iskanja : http://localhost:9200/test_index3/_search?pretty=true&q=PU_ID:1213
+#http://localhost:9200/posebni/_search?pretty=true&q=Leto:2019
 
     # with open('index.json', 'r') as f:
     #     es_mappings = json.loads(f.read())
@@ -58,13 +65,21 @@ actions = []
     #         #print(row)
     #     helpers.bulk(es, c, index='testni_index_2')
 
-TEST_FILENAME = 'C:\ElasticSearch\MT_Projekt\MT_projekt\Podatki\POSEBNI\leto2015.csv'
-test = read_csv(TEST_FILENAME)
+TEST_FILENAME1 = 'C:\ElasticSearch\MT_Projekt\MT_projekt\Podatki\POSEBNI\leto_2015.csv'
+read_csv(TEST_FILENAME1)
+TEST_FILENAME2 = 'C:\ElasticSearch\MT_Projekt\MT_projekt\Podatki\POSEBNI\leto_2016.csv'
+read_csv(TEST_FILENAME2)
+TEST_FILENAME3 = 'C:\ElasticSearch\MT_Projekt\MT_projekt\Podatki\POSEBNI\leto_2017.csv'
+read_csv(TEST_FILENAME3)
+TEST_FILENAME4 = 'C:\ElasticSearch\MT_Projekt\MT_projekt\Podatki\POSEBNI\leto_2018.csv'
+read_csv(TEST_FILENAME4)
+TEST_FILENAME5 = 'C:\ElasticSearch\MT_Projekt\MT_projekt\Podatki\POSEBNI\leto_2019.csv'
+read_csv(TEST_FILENAME5)
 
 # for row in actions:
 #     print(row)
 
-helpers.bulk(es, actions, index='test_index3')
+helpers.bulk(es, actions, index='posebni')
 
 
 #OLD
