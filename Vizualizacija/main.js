751691c49f64fec8dd58a76c3651128981fcca99
klementiaki <klementina.garbajs@gmail.com>
2019-11-30 19:47:52 +0100

    spremembe

--- Vizualizacija/main.js
+++ Vizualizacija/main.js
@@ -3,11 +3,32 @@ mainObj = {};
 
 let showYears = function(){
   for(let key in allYears){
-    console.log(allYears[key].key);
-    document.getElementById('output').innerHTML += "<br>" + "leto " + allYears[key].key;
+    //document.getElementById('output').innerHTML += allYears[key].key;
+
+      var butt=document.createElement("button");
+
+      butt.style.backgroundColor = "#4CAF50";
+      butt.style.border = "none";
+      butt.style.color = "white";
+      butt.style.padding = "20px 55px";
+      butt.style.textAlign = "center";
+      butt.style.display = "inline-block";
+      butt.style.marginRight = "30px";
+      butt.style.fontSize = "30px";
+      butt.style.boxShadow = "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)";
+
+      function changeColor(){
+        this.style.backgroundColor = "#5d98fb";
+        return false;
+      }
+      
+      butt.addEventListener("click", changeColor, false);
+
+      butt.innerHTML = allYears[key].key;
+      document.getElementById("output").appendChild(butt);
+      
   }
 }
-
 
 fetch("data.json")
   .then(function(resp){
@@ -17,4 +38,4 @@ fetch("data.json")
     mainObj = data.aggregations.uniq_gender;
     allYears = mainObj.buckets;
     showYears();
-  })
+})
