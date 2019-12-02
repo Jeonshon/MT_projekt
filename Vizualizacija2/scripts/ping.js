b1dc5a640a3e40a11632588f365efc98cb945255
Jean <jean.marinsek@realis.si>
2019-12-02 13:38:39 +0100

    delujoč prikaz 1 (še vedno potrebnih veliko popravkov)

--- Vizualizacija2/scripts/ping.js
+++ Vizualizacija2/scripts/ping.js
@@ -38,85 +38,294 @@ define(['scripts/d3', 'scripts/elasticsearch'], function (d3, es) {
 // run().catch(console.log)
 console.log("test");
 
-client.search({
-  
+
+//Vrne seštevek zneska za posamezno polizično kategorijo
+client.search({  
   index: 'posebni',
   size: 10000,
   body: {
-      // Begin query.
-      query: {
-        match: {
-             "Leto": "2015" 
-        }
-    },
     aggs: {
-      "leta": {
+      "kategorije": {
         terms: {
-            field: "POL_NAME.keyword",
+          field: "POL_NAME.keyword",
+          order: {
+            val: "desc"
+          },
+          size: 50
+        },
+        aggs: {
+          val: {
+            sum: {
+              field: "Znesek"
+            }
+          }
         }
       }
-    }
+    },
+
+    
+
+    // aggs: {
+    //   "kategorije": {
+    //     terms: {
+    //         field: "POL_NAME.keyword",
+    //     }
+    //   }
+    // },
+    // aggs: {
+    //   "sum":{
+    //     sum: {field: "Znesek"}
+    //   }
+    // }
       // End query.
   }
 }).then(function (resp) {
-      console.log("KOKOKOKOKOKO");
-      //var touchdowns = resp.body.hits.hits.aggregations.POL_NAME.buckets;
-      console.log(resp.hits.hits);
-      console.log(resp.aggregations.leta.buckets);
-      //var touchdowns = resp.body.aggregations.leta.buckets
-      var touchdowns = resp.aggregations.leta.buckets
-      // d3 donut chart
-      var width = 600,
-      height = 300,
-      radius = Math.min(width, height) / 2;
 
-      var color = ['#ff7f0e', '#d62728', '#2ca02c', '#1f77b4'];
+  //console.log(resp.hits.hits);
+  //console.log(resp.aggregations.kategorije.buckets);
 
-      var arc = d3.svg.arc()
-          .outerRadius(radius - 60)
-          .innerRadius(120);
+  var sample = resp.aggregations.kategorije.buckets
+  console.log(sample)
 
-      var pie = d3.layout.pie()
-          .sort(null)
-          .value(function (d) { return d.doc_count; });
+  const svg = d3.select("#container").append("svg")
+  const svgContainer = d3.select('#container').append("svg");
 
-      var svg = d3.select("#donut-chart").append("svg")
-          .attr("width", width)
-          .attr("height", height)
-          .append("g")
-          .attr("transform", "translate(" + width/1.4 + "," + height/2 + ")");
+  const margin = 80;
+    const width = 1000 - 2 * margin;
+    const height = 600 - 2 * margin;
 
-      var g = svg.selectAll(".arc")
-          .data(pie(touchdowns))
-          .enter()
-          .append("g")
-          .attr("class", "arc");
+    const chart = svg.append('g')
+      .attr('transform', `translate(${margin}, ${margin})`);
 
-      g.append("path")
-          .attr("d", arc)
-          .style("fill", function (d, i) {
-              return color[i];
-          });
+    const xScale = d3.scaleBand()
+      .range([0, width])
+      .domain(sample.map((s) => s.key))
+      .padding(0.4)
+    
+    const yScale = d3.scaleLinear()
+      .range([height, 0])
+      .domain([0, sample[0].val.value]);
 
-      g.append("text")
-          .attr("transform", function (d) { return "translate(" + arc.centroid(d) + ")"; })
-          .attr("dy", ".35em")
-          .style("text-anchor", "middle")
-          .style("fill", "white")
-          .text(function (d) { return d.data.key; });
+    // vertical grid lines
+    // const makeXLines = () => d3.axisBottom()
+    //   .scale(xScale)
+
+    const makeYLines = () => d3.axisLeft()
+      .scale(yScale)
+
+    chart.append('g')
+      .attr('transform', `translate(0, ${height})`)
+      .call(d3.axisBottom(xScale));
+
+    chart.append('g')
+      .call(d3.axisLeft(yScale));
+    
+    chart.append('g')
+    .attr('class', 'grid')
+    .call(makeYLines()
+      .tickSize(-width, 0, 0)
+      .tickFormat('')
+    )
+
+    const barGroups = chart.selectAll()
+      .data(sample)
+      .enter()
+      .append('g')
+
+      barGroups
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', (g) => xScale(g.key))
+      .attr('y', (g) => yScale(g.val.value))
+      .attr('height', (g) => height - yScale(g.val.value))
+      .attr('width', xScale.bandwidth())
+      .on('mouseenter', function (actual, i) {
+        d3.selectAll('.val.value')
+          .attr('opacity', 0)
+
+        d3.select(this)
+          .transition()
+          .duration(300)
+          .attr('opacity', 0.6)
+          .attr('x', (a) => xScale(a.key) - 5)
+          .attr('width', xScale.bandwidth() + 10)
+
+        const y = yScale(actual.val.value)
+
+        line = chart.append('line')
+          .attr('id', 'limit')
+          .attr('x1', 0)
+          .attr('y1', y)
+          .attr('x2', width)
+          .attr('y2', y)
+
+        barGroups.append('text')
+          .attr('class', 'divergence')
+          .attr('x', (a) => xScale(a.key) + xScale.bandwidth() / 2)
+          .attr('y', (a) => yScale(a.val.value) + 30)
+          .attr('fill', 'white')
+          .attr('text-anchor', 'middle')
+          .text((a, idx) => {
+            const divergence = (a.val.value - actual.val.value).toFixed(1)
+            
+            let text = ''
+            if (divergence > 0) text += '+'
+            text += `${divergence}%`
+
+            return idx !== i ? text : '';
+          })
+
+      })
+      .on('mouseleave', function () {
+        d3.selectAll('.val.value')
+          .attr('opacity', 1)
+
+        d3.select(this)
+          .transition()
+          .duration(300)
+          .attr('opacity', 1)
+          .attr('x', (a) => xScale(a.key))
+          .attr('width', xScale.bandwidth())
+
+        chart.selectAll('#limit').remove()
+        chart.selectAll('.divergence').remove()
+      })
+
+      barGroups 
+      .append('text')
+      .attr('class', 'value')
+      .attr('x', (a) => xScale(a.key) + xScale.bandwidth() / 2)
+      .attr('y', (a) => yScale(a.val.value) + 30)
+      .attr('text-anchor', 'middle')
+      .text((a) => `${a.val.value}%`)
+
+      svg
+      .append('text')
+      .attr('class', 'label')
+      .attr('x', -(height / 2) - margin)
+      .attr('y', margin / 2.4)
+      .attr('transform', 'rotate(-90)')
+      .attr('text-anchor', 'middle')
+      .text('Love meter (%)')
+
+    svg.append('text')
+      .attr('class', 'label')
+      .attr('x', width / 2 + margin)
+      .attr('y', height + margin * 1.7)
+      .attr('text-anchor', 'middle')
+      .text('kategorije')
+
+    svg.append('text')
+      .attr('class', 'title')
+      .attr('x', width / 2 + margin)
+      .attr('y', 40)
+      .attr('text-anchor', 'middle')
+      .text('sestevek zneska porabe')
+
+    svg.append('text')
+      .attr('class', 'source')
+      .attr('x', width - margin / 2)
+      .attr('y', height + margin * 1.7)
+      .attr('text-anchor', 'start')
+      .text('Source: Stack Overflow, 2018')
 });
 
 
-// const esClient = require('./main');
 
-// esClient.ping({
-// // ping usually has a 3000ms timeout
-//     requestTimeout: Infinity
-// }, function (error) {
-//     if (error) {
-//         console.trace('elasticsearch cluster is down!');
-//     } else {
-//         console.log('All is well');
+
+
+// client.search({
+  
+//   index: 'posebni',
+//   size: 10000,
+//   body: {
+//       // Begin query.
+//       query: {
+//         match: {
+//              "Leto": "2015" 
+//         }
+//     },
+//     aggs: {
+//       "leta": {
+//         terms: {
+//             field: "POL_NAME.keyword",
+//         }
+//       }
 //     }
-// });
+//       // End query.
+//   }
+// }).then(function (resp) {
+//       console.log("KOKOKOKOKOKO");
+//       //var touchdowns = resp.body.hits.hits.aggregations.POL_NAME.buckets;
+//       console.log(resp.hits.hits);
+//       console.log(resp.aggregations.leta.buckets);
+//       console.log(resp.aggregations.leta.buckets.keys);
+
+
+//       var vrednosti = resp.aggregations.leta.buckets;
+//       var sektor = [];
+//       var znesek = [];
+      
+
+//       for(var i in vrednosti){
+//           console.log(vrednosti[i].key);
+//           sektor.push(vrednosti[i].key);
+//           console.log(vrednosti[i].doc_count);
+//           znesek.push(vrednosti[i].doc_count);
+  
+//       }
+
+
+//       var margin = {top: 10, right: 30, bottom: 30, left: 40},
+//     width = 460 - margin.left - margin.right,
+//     height = 400 - margin.top - margin.bottom;
+    
+//       //console.log(sektor);
+//       var svg = d3.select("#donutchart")
+//       .append("svg")
+//         .attr("width", width + margin.left + margin.right)
+//         .attr("height", height + margin.top + margin.bottom)
+//       .append("g")
+//         .attr("transform",
+//               "translate(" + margin.left + "," + margin.top + ")");
+    
+//     // get the data
+//       d = znesek
+    
+//       // X axis: scale and draw:
+//       var x = d3.scaleLinear()
+//           .domain(sektor)     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+//           .range([0, width]);
+//       svg.append("g")
+//           .attr("transform", "translate(0," + height + ")")
+//           .call(d3.axisBottom(x));
+
+
+//       // set the parameters for the histogram
+//       var histogram = d3.histogram()
+//       .value(function(d) { return d; })   // I need to give the vector of value
+//       .domain(x.domain())  // then the domain of the graphic
+//       .thresholds(x.ticks(70)); // then the numbers of bins
+
+
+
+
+
+//       //var touchdowns = resp.body.aggregations.leta.buckets
+//       var touchdowns = resp.aggregations.leta.buckets
+//       // d3 donut chart
+//       var width = 600,
+//       height = 300,
+//       radius = Math.min(width, height) / 2;
+
+//       var color = ['#ff7f0e', '#d62728', '#2ca02c', '#1f77b4'];
+
+
+
+//       // var canvas = d3.select("#donut-chart")
+//       //   .append("svg:svg")
+//       //   .attr("width", '100%')//canvasWidth)
+//       //   .attr("height", '100%');//canvasHeight);
+//   });
+
 });
