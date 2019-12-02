dba1e13201b1137d0c72ba966d4eb2aa0a048c1e
Jean <jean.marinsek@realis.si>
2019-12-02 18:24:57 +0100

    .

--- Vizualizacija2/scripts/ping.js
+++ Vizualizacija2/scripts/ping.js
@@ -62,6 +62,34 @@ client.search({
         }
       }
     },
+    size: 0,
+  _source: {
+    excludes: []
+  },
+  stored_fields: [
+    "*"
+  ],
+  script_fields: {},
+  docvalue_fields: [],
+  query: {
+    bool: {
+      must: [],
+      filter: [
+        {
+          match_all: {}
+        },
+        {
+          match_phrase: {
+            Leto: {
+              query: "2015"
+            }
+          }
+        }
+      ],
+      should: [],
+      must_not: []
+    }
+  }
 
     
 
@@ -88,146 +116,146 @@ client.search({
   console.log(sample)
 
   const svg = d3.select("#container").append("svg")
-  const svgContainer = d3.select('#container').append("svg");
+  //const svgContainer = d3.select('#container').append("svg");
 
-  const margin = 80;
-    const width = 1000 - 2 * margin;
-    const height = 600 - 2 * margin;
+  const margin = 180;
+  const width = 1300 - 2 * margin;
+  const height = 800 - 2 * margin;
 
-    const chart = svg.append('g')
-      .attr('transform', `translate(${margin}, ${margin})`);
+  const chart = svg.append('g')
+    .attr('transform', `translate(${margin}, ${margin})`);
 
-    const xScale = d3.scaleBand()
-      .range([0, width])
-      .domain(sample.map((s) => s.key))
-      .padding(0.4)
-    
-    const yScale = d3.scaleLinear()
-      .range([height, 0])
-      .domain([0, sample[0].val.value]);
+  const xScale = d3.scaleBand()
+    .range([0, width])
+    .domain(sample.map((s) => s.key))
+    .padding(0.4)
+  
+  const yScale = d3.scaleLinear()
+    .range([height, 0])
+    .domain([0, sample[0].val.value]);
 
-    // vertical grid lines
-    // const makeXLines = () => d3.axisBottom()
-    //   .scale(xScale)
+  // vertical grid lines
+  // const makeXLines = () => d3.axisBottom()
+  //   .scale(xScale)
 
-    const makeYLines = () => d3.axisLeft()
-      .scale(yScale)
+  const makeYLines = () => d3.axisLeft()
+    .scale(yScale)
 
-    chart.append('g')
-      .attr('transform', `translate(0, ${height})`)
-      .call(d3.axisBottom(xScale));
+  chart.append('g')
+    .attr('transform', `translate(0, ${height})`)
+    .call(d3.axisBottom(xScale));
 
-    chart.append('g')
-      .call(d3.axisLeft(yScale));
-    
-    chart.append('g')
-    .attr('class', 'grid')
-    .call(makeYLines()
-      .tickSize(-width, 0, 0)
-      .tickFormat('')
-    )
+  chart.append('g')
+    .call(d3.axisLeft(yScale));
+  
+  chart.append('g')
+  .attr('class', 'grid')
+  .call(makeYLines()
+    .tickSize(-width, 0, 0)
+    .tickFormat('')
+  )
 
-    const barGroups = chart.selectAll()
-      .data(sample)
-      .enter()
-      .append('g')
+  const barGroups = chart.selectAll()
+    .data(sample)
+    .enter()
+    .append('g')
 
-      barGroups
-      .append('rect')
-      .attr('class', 'bar')
-      .attr('x', (g) => xScale(g.key))
-      .attr('y', (g) => yScale(g.val.value))
-      .attr('height', (g) => height - yScale(g.val.value))
-      .attr('width', xScale.bandwidth())
-      .on('mouseenter', function (actual, i) {
-        d3.selectAll('.val.value')
-          .attr('opacity', 0)
+    barGroups
+    .append('rect')
+    .attr('class', 'bar')
+    .attr('x', (g) => xScale(g.key))
+    .attr('y', (g) => yScale(g.val.value))
+    .attr('height', (g) => height - yScale(g.val.value))
+    .attr('width', xScale.bandwidth())
+    .on('mouseenter', function (actual, i) {
+      d3.selectAll('.val.value')
+        .attr('opacity', 0)
 
-        d3.select(this)
-          .transition()
-          .duration(300)
-          .attr('opacity', 0.6)
-          .attr('x', (a) => xScale(a.key) - 5)
-          .attr('width', xScale.bandwidth() + 10)
+      d3.select(this)
+        .transition()
+        .duration(300)
+        .attr('opacity', 0.6)
+        .attr('x', (a) => xScale(a.key) - 5)
+        .attr('width', xScale.bandwidth() + 10)
 
-        const y = yScale(actual.val.value)
+      const y = yScale(actual.val.value)
 
-        line = chart.append('line')
-          .attr('id', 'limit')
-          .attr('x1', 0)
-          .attr('y1', y)
-          .attr('x2', width)
-          .attr('y2', y)
+      line = chart.append('line')
+        .attr('id', 'limit')
+        .attr('x1', 0)
+        .attr('y1', y)
+        .attr('x2', width)
+        .attr('y2', y)
 
-        barGroups.append('text')
-          .attr('class', 'divergence')
-          .attr('x', (a) => xScale(a.key) + xScale.bandwidth() / 2)
-          .attr('y', (a) => yScale(a.val.value) + 30)
-          .attr('fill', 'white')
-          .attr('text-anchor', 'middle')
-          .text((a, idx) => {
-            const divergence = (a.val.value - actual.val.value).toFixed(1)
-            
-            let text = ''
-            if (divergence > 0) text += '+'
-            text += `${divergence}%`
+      barGroups.append('text')
+        .attr('class', 'divergence')
+        .attr('x', (a) => xScale(a.key) + xScale.bandwidth() / 2)
+        .attr('y', (a) => yScale(a.val.value) + 30)
+        .attr('fill', 'white')
+        .attr('text-anchor', 'middle')
+        .text((a, idx) => {
+          const divergence = (a.val.value - actual.val.value).toFixed(1)
+          
+          let text = ''
+          if (divergence > 0) text += '+'
+          text += `${divergence}%`
 
-            return idx !== i ? text : '';
-          })
+          return idx !== i ? text : '';
+        })
 
-      })
-      .on('mouseleave', function () {
-        d3.selectAll('.val.value')
-          .attr('opacity', 1)
+    })
+    .on('mouseleave', function () {
+      d3.selectAll('.val.value')
+        .attr('opacity', 1)
 
-        d3.select(this)
-          .transition()
-          .duration(300)
-          .attr('opacity', 1)
-          .attr('x', (a) => xScale(a.key))
-          .attr('width', xScale.bandwidth())
+      d3.select(this)
+        .transition()
+        .duration(300)
+        .attr('opacity', 1)
+        .attr('x', (a) => xScale(a.key))
+        .attr('width', xScale.bandwidth())
 
-        chart.selectAll('#limit').remove()
-        chart.selectAll('.divergence').remove()
-      })
+      chart.selectAll('#limit').remove()
+      chart.selectAll('.divergence').remove()
+    })
 
-      barGroups 
-      .append('text')
-      .attr('class', 'value')
-      .attr('x', (a) => xScale(a.key) + xScale.bandwidth() / 2)
-      .attr('y', (a) => yScale(a.val.value) + 30)
-      .attr('text-anchor', 'middle')
-      .text((a) => `${a.val.value}%`)
+    barGroups 
+    .append('text')
+    .attr('class', 'value')
+    .attr('x', (a) => xScale(a.key) + xScale.bandwidth() / 2)
+    .attr('y', (a) => yScale(a.val.value) + 30)
+    .attr('text-anchor', 'middle')
+    .text((a) => `${a.val.value}%`)
 
-      svg
-      .append('text')
-      .attr('class', 'label')
-      .attr('x', -(height / 2) - margin)
-      .attr('y', margin / 2.4)
-      .attr('transform', 'rotate(-90)')
-      .attr('text-anchor', 'middle')
-      .text('Love meter (%)')
+    svg
+    .append('text')
+    .attr('class', 'label')
+    .attr('x', -(height / 2) - margin)
+    .attr('y', margin / 2.4)
+    .attr('transform', 'rotate(-90)')
+    .attr('text-anchor', 'middle')
+    .text('Love meter (%)')
 
-    svg.append('text')
-      .attr('class', 'label')
-      .attr('x', width / 2 + margin)
-      .attr('y', height + margin * 1.7)
-      .attr('text-anchor', 'middle')
-      .text('kategorije')
+  svg.append('text')
+    .attr('class', 'label')
+    .attr('x', width / 2 + margin)
+    .attr('y', height + margin * 1.7)
+    .attr('text-anchor', 'middle')
+    .text('kategorije')
 
-    svg.append('text')
-      .attr('class', 'title')
-      .attr('x', width / 2 + margin)
-      .attr('y', 40)
-      .attr('text-anchor', 'middle')
-      .text('sestevek zneska porabe')
+  svg.append('text')
+    .attr('class', 'title')
+    .attr('x', width / 2 + margin)
+    .attr('y', 40)
+    .attr('text-anchor', 'middle')
+    .text('sestevek zneska porabe')
 
-    svg.append('text')
-      .attr('class', 'source')
-      .attr('x', width - margin / 2)
-      .attr('y', height + margin * 1.7)
-      .attr('text-anchor', 'start')
-      .text('Source: Stack Overflow, 2018')
+  svg.append('text')
+    .attr('class', 'source')
+    .attr('x', width - margin / 2)
+    .attr('y', height + margin * 1.7)
+    .attr('text-anchor', 'start')
+    .text('Source: Stack Overflow, 2018')
 });
 
 
