c957d336666a4b28aa3aaa1916297c3eb137c79b
Jean <jean.marinsek@realis.si>
2019-12-02 08:10:52 +0100

    delujoč request ki dobi buckete ter vse za leto 2015

--- Vizualizacija2/scripts/ping.js
+++ Vizualizacija2/scripts/ping.js
@@ -1,9 +1,18 @@
-'use strict'
 
 //npm install @elastic/elasticsearch to je treba pognat v terminalu da sploh najde modul
+// const { Client } = require('@elastic/elasticsearch')
+// const client = new Client({ node: 'http://localhost:9200' })
 
-const { Client } = require('@elastic/elasticsearch')
-const client = new Client({ node: 'http://localhost:9200' })
+
+
+
+define(['scripts/d3', 'scripts/elasticsearch'], function (d3, es) {
+  console.log(elasticsearch);
+  "use strict";
+  var client = new elasticsearch.Client();
+  console.log(client);
+
+
 
 // async function run () {
 //   // here we are forcing an index refresh, otherwise we will not
@@ -32,20 +41,69 @@ console.log("test");
 client.search({
   
   index: 'posebni',
-  size: 1,
+  size: 10000,
   body: {
       // Begin query.
       query: {
         match: {
              "Leto": "2015" 
         }
+    },
+    aggs: {
+      "leta": {
+        terms: {
+            field: "POL_NAME.keyword",
+        }
+      }
     }
       // End query.
   }
 }).then(function (resp) {
       console.log("KOKOKOKOKOKO");
       //var touchdowns = resp.body.hits.hits.aggregations.POL_NAME.buckets;
-      console.log(resp.body.hits.hits);
+      console.log(resp.hits.hits);
+      console.log(resp.aggregations.leta.buckets);
+      //var touchdowns = resp.body.aggregations.leta.buckets
+      var touchdowns = resp.aggregations.leta.buckets
+      // d3 donut chart
+      var width = 600,
+      height = 300,
+      radius = Math.min(width, height) / 2;
+
+      var color = ['#ff7f0e', '#d62728', '#2ca02c', '#1f77b4'];
+
+      var arc = d3.svg.arc()
+          .outerRadius(radius - 60)
+          .innerRadius(120);
+
+      var pie = d3.layout.pie()
+          .sort(null)
+          .value(function (d) { return d.doc_count; });
+
+      var svg = d3.select("#donut-chart").append("svg")
+          .attr("width", width)
+          .attr("height", height)
+          .append("g")
+          .attr("transform", "translate(" + width/1.4 + "," + height/2 + ")");
+
+      var g = svg.selectAll(".arc")
+          .data(pie(touchdowns))
+          .enter()
+          .append("g")
+          .attr("class", "arc");
+
+      g.append("path")
+          .attr("d", arc)
+          .style("fill", function (d, i) {
+              return color[i];
+          });
+
+      g.append("text")
+          .attr("transform", function (d) { return "translate(" + arc.centroid(d) + ")"; })
+          .attr("dy", ".35em")
+          .style("text-anchor", "middle")
+          .style("fill", "white")
+          .text(function (d) { return d.data.key; });
 });
 
 
@@ -61,3 +119,4 @@ client.search({
 //         console.log('All is well');
 //     }
 // });
+});
